c05ff0bc739d7169bf12c15712a1b4a224591ad2
NurGul <nurgul.94@mail.ru>
2013-11-11 15:10:28 +0600

    first

--- server/src/main/java/hello/Application.java
+++ server/src/main/java/hello/Application.java
@@ -4,14 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-
+        RestTemplate restTemplate = new RestTemplate();
+        Customer[] customers = restTemplate.getForObject("http://192.168.1.69:8080/users/", Customer[].class);
+        for (Customer customerElement : customers){
+            System.out.println("ID:    " + customerElement.getId());
+            System.out.println("firstName:   " + customerElement.getFirstName());
+            System.out.println("lastName:   " + customerElement.getLastName());
+            System.out.println(" Эмне кылгыныз келип жатат ? ");
+            System.out.println(" 1 - Дүкөндөрдүн тизмесин чыгаруу ");
+            System.out.println(" 2 - Тизмедеги дүкөндү өзгөртүү ");
 
         SpringApplication.run(Application.class, args);
+    }
     }
 }
