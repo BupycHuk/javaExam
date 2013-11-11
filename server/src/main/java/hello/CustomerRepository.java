311605bff292613627e896c06605685f42e4afb1
pe4en <kalysabdyldaev@gmail.com>
2013-11-11 15:45:04 +0600

    Update CustomerRepository.java

--- server/src/main/java/hello/CustomerRepository.java
+++ server/src/main/java/hello/CustomerRepository.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-    List<Customer> findByLastName(String providerName);
+    List<Customer> findByProviderName(String ProviderName);
 }
 
